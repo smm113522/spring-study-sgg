bb9d2b2f52a9ad42ea1a4529f6de77e9717ee8e2
樂觀 <908085643@qq.com>
2022-05-23 08:50:13 +0800

    学习初始化

---
+++ spring-i18/src/main/java/com/spring/i18/I18Application.java
@@ -0,0 +1,13 @@
+package com.spring.i18;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class I18Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(I18Application.class, args);
+    }
+
+}
